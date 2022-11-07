db3a44292384f3b0f122ac5c06a95f8d6bedd628
kkukri <klaustanpj29@gmail.com>
2022-11-07 18:01:13 +0800

    Updates MovieManager class, still work in progress.

--- src/boundaries/movie/MovieMainMenuUI.java
+++ src/boundaries/movie/MovieMainMenuUI.java
@@ -1,16 +1,8 @@
 package boundaries.movie;
 
-public class MovieMasterMenu {
-    private static MovieMasterMenu single_instance = null;
-
-    public static MovieMasterMenu getInstance() {
-        if (single_instance == null)
-            single_instance = new MovieMasterMenu();
-        return single_instance;
-    }
-
-    public void masterMenuStaff() {
-            System.out.println("------------------- MOVIE MASTER CONTROL (STAFF) ------------------\n" +
+public class MovieMainMenuUI {
+    public static void printStaffMenu() {
+            System.out.println("------------------- MOVIE MAIN MENU (STAFF) ------------------\n" +
                     " 1. View/ Edit Movies 						    		\n" +
                     " 2. Add Movies		                                 	\n" +
                     " 3. Search Movies (By Title)	                        \n" +
@@ -19,5 +11,7 @@ public class MovieMasterMenu {
 
             System.out.println("Enter choice: ");
     }
+
+    // CUST
 
 }
